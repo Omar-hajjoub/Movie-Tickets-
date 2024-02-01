b2b46eefe393201f96855bf62edc2d9919f70cac
OMAR HAJJOUB <135070760+Omar-hajjoub@users.noreply.github.com>
2024-02-01 23:13:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 
+let seting,seats,total,movie,Reserved,add,titeprice
+ seting =document.querySelector('.seting')
+ seats=document.querySelectorAll('.thisbox .sate')
+  total =document.getElementById('totol')
+ movie =document.getElementById('movie')
+ Reserved = document.getElementById('Reserved')
+ add =document.getElementById('add')
+ titeprice = +movie.value
 
-let seting =document.querySelector('.seting')
-let seats=document.querySelectorAll('.thisbox .sate')
-let total =document.getElementById('totol')
-let movie =document.getElementById('movie')
-let Reserved = document.getElementById('Reserved')
-let add =document.getElementById('add')
+ let tmp ;
+// array localstokage 
+let dataresevrt;
 
-let titeprice = +movie.value
+if(localStorage.RervieMovie != null){
+    dataresevrt = JSON.parse(localStorage.RervieMovie)
+   
+   
+}else{
+    dataresevrt =[]; 
+}
+
+console.log(tmp)
+
+/////----------------------------
 function updatselector() {
     const selectore = document.querySelectorAll('.thisbox .sate.selectd')
     const numberselector = +selectore.length
@@ -20,61 +35,125 @@ function updatselector() {
 
 }
 
-
+// Movie add
 movie.addEventListener('click', (e) =>{
     titeprice = +e.target.value;
     updatselector()
 })
 
 
-
+//------------------------------------------------
 seting.addEventListener('click', (e)=>{
+   
     if(e.target.classList.contains('sate') && !e.target.classList.contains('occupied')){
 e.target.classList.toggle('selectd')
     }
 
-
+    tmp=e
     updatselector()
 })
 
 
-add.addEventListener('click', ()=>{
-    var name , email,phone,movie,reserved,ToTal,card
-    name =document.querySelector('.name')
+
+
+
+
+
+
+
+
+
+
+
+
+
+// input Carrdt select
+var Name , email,phone,movie1,reserved,ToTal
+    Name =document.querySelector('.name')
     email =document.querySelector('.email')
     phone =document.querySelector('.phone')
-    movie =document.querySelector('.movie')
+    movie1 =document.querySelector('.movie')
     reserved =document.querySelector('.reserved')
     ToTal =document.querySelector('.ToTal')
 
 
-    name.innerHTML = document.getElementById('Name').value
+    /// input element
+    var  NAM,EM,Ph,MOV,RESRFT,TOT
+        NAM=  document.getElementById('Name')
+         EM  = document.getElementById('email')
+          Ph  = document.getElementById('phone')
+          MOV  = document.getElementById('movie')
+         RESRFT =  document.getElementById('Reserved')
+        TOT   = document.getElementById('totol')
+
+
+
+//------------------------------------------------------------
+ function addSeat(){
+    if(NAM.value !== '' && EM.value !== '' && Ph.value !== '' && RESRFT.value !=='' && TOT.value !=='' ){
+        Name.innerHTML = NAM.value //Name
+        email.innerHTML = EM.value //email
+        phone.innerHTML = Ph.value //phone
+        movie1.innerHTML = MOV.value //movie
+        reserved.innerHTML = RESRFT.value // Reserved
+        ToTal.innerHTML = TOT.value +"$" //Total
+        card=document.querySelector('.card').style.top='10%'
+    }else{
+    alert('Plaise Resrvie Movie ')
+    
+    }
+     
+    
+        scrollTo({
+            top: 0,
+            behavior: 'smooth' // إضافة تأثير ناعم للتمرير
+          });
+ }
+
+
+
+
+  
+
+
+
+//------------------------------------------------------------
+function confrmt(){
+    
+    Name.innerHTML = document.getElementById('Name').value
     email.innerHTML = document.getElementById('email').value
     phone.innerHTML = document.getElementById('phone').value
-    movie.innerHTML = document.getElementById('movie').value
+    movie1.innerHTML = document.getElementById('movie').value
     reserved.innerHTML = document.getElementById('Reserved').value
     ToTal.innerHTML = document.getElementById('totol').value+" $"
 
-    card=document.querySelector('.card').style.top='10%'
+  //Data arry and Object  
+let userprodect ={
+name:Name.innerHTML,
+email:email.innerHTML,
+phone:phone.innerHTML,
+film:movie1.innerHTML,
+reserved:reserved.innerHTML,
+total:ToTal.innerHTML
 
-    scrollTo({
-        top: 0,
-        behavior: 'smooth' // إضافة تأثير ناعم للتمرير
-      });
-
-  
-})
+}
+// data localstokage
+   dataresevrt.push(userprodect)
+   localStorage.setItem("RervieMovie", JSON.stringify(dataresevrt))
 
 
-function confrmt(){
+
     let sir= document.querySelector('.sir')
     document.querySelector('.card').style.top='-100%'
     document.querySelector('.cardconfirm').style.top='10%'
     sir.innerHTML = document.getElementById('Name').value
+
+
     setTimeout(function(){location.reload()},3000)
    
 
 }
+//------------------------------------------------------------
 function cancel(){
     document.querySelector('.card').style.top='-100%'
 }
